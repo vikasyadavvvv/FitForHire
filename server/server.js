325b7bba1ea80840e63f2ea08f63db0ddb6452fb
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2025-06-07 08:52:21 +0530

    CORS UPDATE

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import resumeRoutes from "./routes/resume.js";
 import fileUpload from "express-fileupload";
+import path from 'path'
 
 
 dotenv.config();
@@ -19,6 +20,17 @@ app.use(
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
+app.use(cors({
+  origin: [
+    "https://fit-for-hire-livid.vercel.app/", // Production frontend
+    "http://localhost:5173"             // Local Vite dev server
+  ],
+  credentials: true
+}));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"));
 
