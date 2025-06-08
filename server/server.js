36499eba1337166afe120b7ff1e2565e7caeb885
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2025-06-08 16:02:55 +0530

    UPDATEDSSS

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/resume", resumeRoutes);
 
 app.get('/',(req,res)=>{
-  res.send("Backend is running")
+  res.json({message:"Backend is Running"})
 })
 
 
