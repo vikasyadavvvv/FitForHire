3f3e8c521bff9c3a0fd57a0388bcb807aabdc1e8
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2025-07-09 00:47:32 +0530

    backend allowed origin

--- server/server.js
+++ server/server.js
@@ -18,7 +18,11 @@ app.use(
   })
 );
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://fit-for-hire-livid.vercel.app', 
+  methods: ['GET', 'POST', 'OPTIONS'],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
