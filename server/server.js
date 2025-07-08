74606aab42a284e2ba7d7aa9b0ea243e8efb316c
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2025-07-09 01:11:01 +0530

    Server update

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ import resumeRoutes from "./routes/resume.js";
 dotenv.config();
 const app = express();
 
-// Clerk middleware
 
+app.use(cors())
 // File upload config
 app.use(
   fileUpload({
@@ -18,16 +18,7 @@ app.use(
   })
 );
 
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://fit-for-hire-livid.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'OPTIONS'],
-  credentials: true
-}));
 
-app.options('*', cors()); // very important!
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
